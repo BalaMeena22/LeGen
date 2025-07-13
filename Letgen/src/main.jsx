3e5e63ad135e4863fab3bcdb48f99e2e43ec4001
BalaMeena2205 <balameenaaseenivasan@gmail.com>
2025-07-13 19:24:07 +0530

    Add missing dependencies and fix build

--- Letgen/src/main.jsx
+++ Letgen/src/main.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
 )
