196c3efcb25a87eef4b4a7089142f735a1a72844
BalaMeena2205 <balameenaaseenivasan@gmail.com>
2025-07-13 19:19:30 +0530

    Add missing dependencies and fix build

--- Letgen/src/App.jsx
+++ Letgen/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signup from './Signup';  
 import Login from './Login';  
 import Dashboard from './Dashboard';
-import 'bootstrap/dist/css/bootstrap.min.css';  
 
 function App() {  
   return (  
